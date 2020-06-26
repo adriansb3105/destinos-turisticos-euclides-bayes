b6e896ad25aea57ceb58b8f0e48e497a7031bc60
Adrián Serrano <adriansb3105@gmail.com>
2020-06-26 12:43:02 -0600

    Cambios en la configuracion de la base de datos

--- app/Http/Controllers/AtractivosAppController.php
+++ app/Http/Controllers/AtractivosAppController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Helpers\Algoritmo\Euclides;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Input;
 
 class AtractivosAppController extends Controller
 {
